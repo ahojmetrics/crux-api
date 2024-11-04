70b8e7a3af21c84f611aba82a4321f4a57c98e3b
Yuri Tománek <yuri@tomanek.co.uk>
2024-11-04 22:10:06 +0000

    feat: cache api

--- lib/crux_api/client.rb
+++ lib/crux_api/client.rb
@@ -2,7 +2,7 @@ require "httparty"
 
 class CruxApi::Client
   # frozen_string_literal: true
- 
+
   API_URL = "https://chromeuxreport.googleapis.com/v1/records"
 
   def initialize
@@ -10,33 +10,48 @@ class CruxApi::Client
   end
 
   def get(url)
-    return { error: "No API key found" } unless @api_key
+    return {error: "No API key found"} unless @api_key
 
-    response = HTTParty.post(
-      "#{API_URL}:queryRecord?key=#{@api_key}",
-      query: {
-        url: url
-      }
-    )
+    Rails.cache.fetch("#{cache_key(api_type: "rolling", url: url)}", expires_in: 12.hours) do
+      puts "Uncached: Fetching from API"
 
-    response.parsed_response
+      response = HTTParty.post(
+        "#{API_URL}:queryRecord?key=#{@api_key}",
+        query: {
+          url: url
+        }
+      )
+
+      response.parsed_response
+    end
   end
 
   def history(url)
-    return { error: "No API key found" } unless @api_key
+    return {error: "No API key found"} unless @api_key
 
-    response = HTTParty.post(
-      "#{API_URL}:queryHistoryRecord?key=#{@api_key}",
-      query: {
-        url: url,
-        formFactor: "PHONE",
-      },
-      headers: {
-        "Content-Type" => "application/json",
-        "Accept" => "application/json",
-      }
-    )
-    
-    response.parsed_response
+    Rails.cache.fetch("#{cache_key(api_type: "history", url: url)}", expires_in: 12.hours) do
+      puts "Uncached: Fetching from API"
+
+      response = HTTParty.post(
+        "#{API_URL}:queryHistoryRecord?key=#{@api_key}",
+        query: {
+          url: url,
+          formFactor: "PHONE"
+        },
+        headers: {
+          "Content-Type" => "application/json",
+          "Accept" => "application/json"
+        }
+      )
+
+      response.parsed_response
+    end
+  end
+
+  private
+
+  def cache_key(api_type:, url:)
+    url = url.gsub("https://", "").gsub("http://", "")
+    "crux_#{api_type}_api_#{url}"
   end
 end
